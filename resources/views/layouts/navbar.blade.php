f57405baf0bda979483cb19eb86b2a378cbf3498
jose1193 <josegonzalezcr2794@gmail.com>
2022-10-19 01:10:34 -0300

    commit

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -137,6 +137,31 @@
                   </div>
               </li>
                 
+              <li>
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar3" class=" capitalize flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                  food market<svg class="ml-1 w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                <!-- Dropdown menu -->
+                <div id="dropdownNavbar3" class=" capitalize hidden z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                      <li>
+                        <a href="/marketcompanies" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white
+                        capitalize"> market companies </a>
+                      </li>
+                      <li>
+                        <a href="/" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white
+                        capitalize">menu 1 </a>
+                      </li>
+
+                      <li>
+                        <a href="/" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white
+                        capitalize">menu 2 </a>
+                      </li>
+                     
+                    </ul>
+                   
+                </div>
+            </li>
+
                   <li>
                       <a href="/emails" class="block py-2 pr-4 pl-3 text-gray-700 border-b
                        border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0
